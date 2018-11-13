b5dc34c32db2b7b7e2e242f1e53d109b57810b71
toubill <44949228+toubill@users.noreply.github.com>
2018-11-13 17:01:03 +0100

    added Bong

--- item.sql
+++ item.sql
@@ -1,15 +1,17 @@
--------------------------------------------- Ajout item 
-
 INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+('bong', 'bang', 1, 0, 1),
 ('umbrellablue', 'Parapluie bleu', 1, 0, 1),
 ('umbrellagrey', 'Parapluie gris', 1, 0, 1),
 ('handbag', 'Sac a main (serviette)', 1, 0, 1),
 ('handbagtacticgrey', 'valise tactic gris', 1, 0, 1),
 ('handbagholiday', 'valise vacance en tissu', 1, 0, 1),
-('handbagholidayhard', 'valise vacance résistante', 1, 0, 1);
+('handbagholidayhard', 'valise vacance résistante', 1, 0, 1),
+
 
 
 INSERT INTO `shops` (`name`, `item`, `price`) VALUES 
+('TwentyFourSeven', 'bong', '1'), 
+('RobsLiquor', 'bong', '1'), 
 ('TwentyFourSeven', 'umbrellablue', '1'), 
 ('TwentyFourSeven', 'umbrellagrey', '1'), 
 ('TwentyFourSeven', 'handbag', '1'), 
@@ -23,10 +25,7 @@ INSERT INTO `shops` (`name`, `item`, `price`) VALUES
 ('RobsLiquor', 'handbagholiday', '1'), 
 ('RobsLiquor', 'handbagholidayhard', '1');
 
-
-
-			--PROPLIST--
-			
+        --PROPLIST--			
 --prop_security_case_01 -- valise tactic gris reistant
 --prop_idol_case_02 -- valise noir resistante
 --prop_ld_case_01 -- valise avocat

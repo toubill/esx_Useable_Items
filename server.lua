fe6b51176f88297dbced40c270994037acbfd891
toubill <44949228+toubill@users.noreply.github.com>
2018-11-13 17:00:24 +0100

    added bong

--- server.lua
+++ server.lua
@@ -43,3 +43,51 @@ ESX.RegisterUsableItem('handbag', function(source)
 	local xPlayer  = ESX.GetPlayerFromId(source)
 	--xPlayer.removeInventoryItem('handbag', 1)   --- decommenter pour que l'items se supprime a chaque utilisation
 end)
+---BONG---
+ESX.RegisterUsableItem('bong', function(source)
+local xPlayer  = ESX.GetPlayerFromId(source)
+
+	if xPlayer.getInventoryItem('weed_pooch').count <= 0 then
+		weedpooch = 0
+	else
+		weedpooch = 1
+	end
+			
+	if xPlayer.getInventoryItem('pharma3').count <= 0 then
+		weedmedicinal = 0
+	else
+		weedmedicinal = 1
+	end
+			
+	if weedpooch == 0 and weedmedicinal == 0 then	
+		TriggerClientEvent('esx:showNotification', source, 'Vous n\'avez ~r~pas de ~g~weed ~w~ou de ~b~cannabis medicinal~w~ à consommer')
+		return
+	elseif 
+		xPlayer.getInventoryItem('weed_pooch').count <= 1 and weedmedicinal == 0 then
+		TriggerClientEvent('esx:showNotification', source, 'Vous n\'avez ~r~pas de Weed~w~ à consommer')
+		weedpooch = 0
+		return
+	elseif 
+		xPlayer.getInventoryItem('pharma3').count <= 1 and weedpooch == 0 then
+		TriggerClientEvent('esx:showNotification', source, 'Vous n\'avez ~r~pas de Weed Médicinal~w~ à consommer')
+		weedmedicinal = 0
+		return
+	elseif (weedpooch == 1) then		
+		TriggerClientEvent('esx_extraitem:bong', source)
+		local xPlayer  = ESX.GetPlayerFromId(source)
+		--xPlayer.removeInventoryItem('bong', 1) --- decommenter pour que l'items se supprime a chaque utilisation
+		xPlayer.removeInventoryItem('weed_pooch', 1)
+		TriggerClientEvent('esx_status:add', source, 'drug', 350000)
+		TriggerClientEvent('esx:showNotification', source, '~g~Wow ! C\'est d\'l\'a bonne')
+	elseif (weedmedicinal == 1) then
+		TriggerClientEvent('esx_extraitem:bong', source)
+		local xPlayer  = ESX.GetPlayerFromId(source)
+		--xPlayer.removeInventoryItem('bong', 1) --- decommenter pour que l'items se supprime a chaque utilisation
+		xPlayer.removeInventoryItem('pharma3', 1)
+		TriggerClientEvent('esx_status:add', source, 'drug', 10000)
+		TriggerClientEvent('esx:showNotification', source, '~g~Wow ! C\'est d\'l\'a bonne')
+	end		
+end)	
+
+
+
